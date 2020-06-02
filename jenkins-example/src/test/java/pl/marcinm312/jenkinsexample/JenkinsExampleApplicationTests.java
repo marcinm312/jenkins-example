e2a663f713c7c0614c3704a5d30fd5f3c1e48f43
Marcin Michalczyk <marcinm312@outlook.com>
2020-06-03 01:14:42 +0200

    test fix

--- jenkins-example/src/test/java/pl/marcinm312/jenkinsexample/JenkinsExampleApplicationTests.java
+++ jenkins-example/src/test/java/pl/marcinm312/jenkinsexample/JenkinsExampleApplicationTests.java
@@ -1,5 +1,7 @@
 package pl.marcinm312.jenkinsexample;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,12 @@ class JenkinsExampleApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void added() {
+		JenkinsExampleApplication jexampleApplication = new JenkinsExampleApplication();
+		assertEquals(10, jexampleApplication.added(5, 5));
 	}
 
 }
