9423220d0662aad2ed0356b9d11818da7fe8db1a
Marcin Michalczyk <marcinm312@outlook.com>
2020-06-04 00:12:45 +0200

    Adding example Rest method and test

--- jenkins-example/src/main/java/pl/marcinm312/jenkinsexample/JenkinsExampleApplication.java
+++ jenkins-example/src/main/java/pl/marcinm312/jenkinsexample/JenkinsExampleApplication.java
@@ -2,8 +2,11 @@ package pl.marcinm312.jenkinsexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsExampleApplication {
 
 	public static void main(String[] args) {
@@ -14,4 +17,8 @@ public class JenkinsExampleApplication {
 		return a + b;
 	}
 
+	@GetMapping
+	public String get() {
+		return "hello";
+	}
 }
--- jenkins-example/src/test/java/pl/marcinm312/jenkinsexample/JenkinsExampleApplicationTests.java
+++ jenkins-example/src/test/java/pl/marcinm312/jenkinsexample/JenkinsExampleApplicationTests.java
@@ -1,12 +1,23 @@
 package pl.marcinm312.jenkinsexample;
 
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JenkinsExampleApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
@@ -16,6 +27,11 @@ class JenkinsExampleApplicationTests {
 	public void added() {
 		JenkinsExampleApplication jexampleApplication = new JenkinsExampleApplication();
 		assertEquals(10, jexampleApplication.added(5, 5));
+	}
+
+	@Test
+	void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string("hello"));
 	}
 
 }
